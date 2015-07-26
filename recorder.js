ebe028bc2d468da7ec41c946ca11c9ef0914fa36
Felipe Astroza Araya <felipe@astroza.cl>
2015-07-25 22:22:48 -0300

    test page

--- recorder.js
+++ recorder.js
@@ -32,8 +32,10 @@ var worker = new Worker('ogg_encoder_worker.js');
 
 		worker.onmessage = function(e){
 			window.blob = new Blob([new Uint8Array(e.data.buffer, 0, e.data.outputLength)], { type: 'audio/ogg' });
-			document.getElementById('audioOgg').href = window.URL.createObjectURL(window.blob);
-			document.getElementById('audioOgg').download = "audio.ogg";
+			download_link = document.getElementById('audioOgg');
+			download_link.href = window.URL.createObjectURL(window.blob);
+			download_link.download = "audio.ogg";
+			download_link.style.display = "block";
 		}
 
 		source.connect(this.node);
@@ -41,5 +43,4 @@ var worker = new Worker('ogg_encoder_worker.js');
 	};
 
 	window.Recorder = Recorder;
-
 })(window);
